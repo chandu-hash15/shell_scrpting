7735aa239871c99557c4268af5fad5b2336c7418
Chandu <chandutharun42@gmail.com>
2025-09-26 00:20:35 +0530

    lets build our convo

---
+++ 02_conersation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+var1= $1
+var2= $2
+echo "$var1 : hello $var2, how are you?"
+echo "$var2 : I am fine, thank you! $var1. nice to meet you!"
+echo "$var1 : nice to meet you too, $var2!"
+echo "End of conversation."
