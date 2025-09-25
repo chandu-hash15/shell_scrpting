5b739cba1478a99ca235120c60e1ea451e658b31
Chandu <chandutharun42@gmail.com>
2025-09-26 00:50:19 +0530

    done with the changes 08

--- 02_conersation.sh
+++ 02_conersation.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+
 var1=$1
 var2=$2
+
 echo "$var1 : hello $var2, how are you?"
 echo "$var2 : I am fine, thank you! $var1. nice to meet you!"
 echo "$var1 : nice to meet you too, $var2 .,;!"
