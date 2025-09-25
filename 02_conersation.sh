d668ae107cb71705be31345d3e386576382243da
Chandu <chandutharun42@gmail.com>
2025-09-26 00:41:22 +0530

    done with the changes 05

--- 02_conersation.sh
+++ 02_conersation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-person1 = $1
-person2 = $2
+person1 = sriram
+person2 = ram
 echo "$person1 : hello $person2, how are you?"
 echo "$person2 : I am fine, thank you! $person1. nice to meet you!"
 echo "$person1 : nice to meet you too, $person2 .,;!"
