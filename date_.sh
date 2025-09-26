5ac1a275591059abf9ac3df3def7b85c4a42470f
Chandu <chandutharun42@gmail.com>
2025-09-26 11:11:22 +0530

    total time taken

---
+++ date_.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+start_time=$(date +%s)
+
+slepp 10
+
+end_time=$(date +%s)
+
+Total_time=$(end_time - start_time)
+
+echo "Total time taken to execute the script is: $Total_time seconds"
