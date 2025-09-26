487bd477d8747b713b35611504a3770a68263c39
Chandu <chandutharun42@gmail.com>
2025-09-26 14:01:54 +0530

    installing mysql

---
+++ exit_status.sh
@@ -0,0 +1,16 @@
+
+USER_ID = $(id -u)
+if [ $USER_ID -ne 0 ] ; then
+    echo "you should have root privileges to run this script"
+    exit 1
+else
+    echo "you are root user"
+fi  
+dnf install mysql -y
+if [ $? -eq 0 ]; then
+    echo "mysql installed successfully"
+else
+    echo "mysql installation failed"
+    exit 1
+fi
+
