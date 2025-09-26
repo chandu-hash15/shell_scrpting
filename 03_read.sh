aa4f40a2f60500a35d89fa50d0ed085bf18692bf
Chandu <chandutharun42@gmail.com>
2025-09-26 10:21:30 +0530

    HDFC SERVICES PIN CONFIRMATION-6

--- 03_read.sh
+++ 03_read.sh
@@ -5,11 +5,10 @@ echo "Please enter your ATM pin:"
 read PIN_NUMBER
 echo "re-enter your pin:"
 read ATM_PIN
-if ["($PIN_NUMBER eq $ATM_PIN)"];
+if ["($PIN_NUMBER ne $ATM_PIN)"];
 then
-  echo "Your pin is correct"
-else
   echo "Your pin is incorrect, please try again"
-  exit 1
+else
+  echo "Your pin is correct, proceeding..."
 fi
 echo "Thank you for using HDFSC services"
