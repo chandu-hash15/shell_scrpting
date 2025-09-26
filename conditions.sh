58848027da3e782809051d6c3f010410d23c3c26
Chandu <chandutharun42@gmail.com>
2025-09-26 13:42:49 +0530

    conditions 01

--- conditions.sh
+++ conditions.sh
@@ -3,7 +3,8 @@
 var1=$1
 if [ $var1 -gt 10 ];then
     echo "$var1 is greater than 10"
-
+elif [ $var1 -eq 10 ];then
+    echo "$var1 is equal to 10"
 else
     echo "$var1 is less than or equal to 10"
 fi
