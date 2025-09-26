8bebb10b4fa8c1f971066ebacfa0e67300d8cc78
Chandu <chandutharun42@gmail.com>
2025-09-26 12:08:33 +0530

    specail variables 3

--- special-variables.sh
+++ special-variables.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 echo "many variables are special variables:$@"
 echo "this is the user id of the current user :$UID"
+echo "PID  of this script is :$$"
+sleep 10 &
+echo "PID of the last background process is :$!" # this is useful to check if the background process is running or not
