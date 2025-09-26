0b1da3bd75eedc2589b72dff8950fac5c0fcf9b5
Chandu <chandutharun42@gmail.com>
2025-09-26 09:55:54 +0530

    HDFC SERVICES

---
+++ 03_read.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Thanks for using HDFSC services"
+echo "Please enter your ATM pin:"
+echo "re-enter your pin:"
+read PIN_NUMBWER
+echo "Your ATM pin is: $PIN_NUMBWER"
+echo "Thank you for using HDFSC services"
