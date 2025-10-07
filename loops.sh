799512f8a64721c154d0062394fb8350b86b5e03
Chandu <chandutharun42@gmail.com>
2025-10-07 12:10:53 +0530

    working on loops

---
+++ loops.sh
@@ -0,0 +1,40 @@
+#!/bin/bas
+
+R = "\e[31m"
+G = "\e[32m"
+Y = "\e[33m"
+N = "\e[0m"
+
+
+LOG_FOLDER= "/var/log/sell_script"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE= "$LOG_FOLDER/$SCRIPT_NAME.log" #/var/log/shell_script/loops.log
+
+USER_ID=$(id)
+if [ $USER_ID -ne 0 ]; then
+    echo " you ate not a root user "
+    exit 1
+else
+    echo " you are root user "
+fi
+valiidate() {
+    if [ $? -ne 0 ]; then
+        echo -e " $2 installation failed $R installation failed $N" 
+       exit 1
+    else
+        echo -e " installaltion $2 ----$G successfull $N"
+    fi
+}
+
+mkdir -p $LOG_FOLDER
+
+for $tools in $@;
+do
+    dnf list installed $tools &>> $LOG_FILE
+    if [ $? ne 0 ]; then
+        dnf install $tools -y &>> $LOG_FILE
+        validate $? "$tools"
+    else
+        echo -e "$tools already installed $Y SHIPPING $N"
+    fi
+done
